3a1458ac9d598c3d104dba8c003fdf004853e600
Jacob Ruiz <jacobruizdesign@gmail.com>
2020-04-03 17:09:03 +1000

    works

---
+++ components/heading/index.js
@@ -0,0 +1,9 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
+import { Heading as Element } from 'theme-ui';
+
+const Heading = (props) => {
+  return <Element {...props} />;
+};
+
+export default Heading;
---
+++ themes/qordoba.js
@@ -0,0 +1,172 @@
+const calcMarginTop = (lineHeight) => {
+  const capitalHeight = 0.3;
+  const toTrim = (lineHeight - capitalHeight) * -0.5;
+  return `${toTrim}em`;
+};
+
+const trimWhitespace = {
+  transform: `translateY(0.25em)`,
+  '::before': {
+    content: `''`,
+    display: 'block',
+    opacity: 0,
+    height: 0,
+    width: '100%',
+    marginBottom: `${calcMarginTop(1.1)}`
+  }
+};
+
+const theme = {
+  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+  fonts: {
+    body:
+      '"Poppins", system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
+    heading: 'inherit',
+    monospace: 'Menlo, monospace'
+  },
+  fontSizes: [10, 12, 13, 18, 30, 40, 48, 64, 96],
+  fontWeights: {
+    body: 400,
+    heading: 600,
+    bold: 700
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.1
+  },
+  colors: {
+    text: '#000',
+    background: '#fff',
+    primary: '#0060FF',
+    secondary: '#30c',
+    muted: '#f6f6f6'
+  },
+  styles: {
+    root: {
+      fontFamily: 'body',
+      lineHeight: 'body',
+      fontWeight: 'body'
+    },
+    h1: {
+      color: 'text',
+      fontFamily: 'heading',
+      lineHeight: 'heading',
+      fontWeight: 'heading',
+      fontSize: 5
+    },
+    h2: {
+      color: 'text',
+      fontFamily: 'heading',
+      lineHeight: 'heading',
+      fontWeight: 'heading',
+      fontSize: 4
+    },
+    h3: {
+      color: 'text',
+      fontFamily: 'heading',
+      lineHeight: 'heading',
+      fontWeight: 'heading',
+      fontSize: 3
+    },
+    h4: {
+      color: 'text',
+      fontFamily: 'heading',
+      lineHeight: 'heading',
+      fontWeight: 'heading',
+      fontSize: 2
+    },
+    h5: {
+      color: 'text',
+      fontFamily: 'heading',
+      lineHeight: 'heading',
+      fontWeight: 'heading',
+      fontSize: 1
+    },
+    h6: {
+      color: 'text',
+      fontFamily: 'heading',
+      lineHeight: 'heading',
+      fontWeight: 'heading',
+      fontSize: 0
+    },
+    p: {
+      color: 'text',
+      fontFamily: 'body',
+      fontWeight: 'body',
+      lineHeight: 'body'
+    },
+    a: {
+      color: 'primary'
+    },
+    pre: {
+      fontFamily: 'monospace',
+      overflowX: 'auto',
+      code: {
+        color: 'inherit'
+      }
+    },
+    code: {
+      fontFamily: 'monospace',
+      fontSize: 'inherit'
+    },
+    table: {
+      width: '100%',
+      borderCollapse: 'separate',
+      borderSpacing: 0
+    },
+    th: {
+      textAlign: 'left',
+      borderBottomStyle: 'solid'
+    },
+    td: {
+      textAlign: 'left',
+      borderBottomStyle: 'solid'
+    },
+    img: {
+      maxWidth: '100%'
+    }
+  },
+
+  // Variants
+  display: {
+    1: {
+      fontSize: 8,
+      ...trimWhitespace
+    },
+    2: {
+      fontSize: 7,
+      ...trimWhitespace
+    },
+    3: {
+      fontSize: 6,
+      ...trimWhitespace
+    }
+  },
+  heading: {
+    1: {
+      fontSize: 5,
+      ...trimWhitespace
+    },
+    2: {
+      fontSize: 4,
+      ...trimWhitespace
+    },
+    3: {
+      fontSize: 3,
+      ...trimWhitespace
+    },
+    4: {
+      fontSize: 2,
+      ...trimWhitespace
+    },
+    5: {
+      fontSize: 0,
+      letterSpacing: 1.3,
+      fontWeight: 700,
+      textTransform: 'uppercase',
+      ...trimWhitespace
+    }
+  }
+};
+
+export default theme;
