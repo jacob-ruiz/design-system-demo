fad758d360f4b7a5d858e8175fb861ae7a4e2a42
Jacob Ruiz <jacobruizdesign@gmail.com>
2020-04-07 18:07:22 +1000

    add google font link

--- playroom/FrameComponent.js
+++ playroom/FrameComponent.js
@@ -4,6 +4,12 @@ import theme from '../themes/qordoba';
 
 export default ({ children }) => (
   <ThemeProvider theme={theme}>
-    <Styled.root>{children}</Styled.root>
+    <>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap"
+        rel="stylesheet"
+      />
+      <Styled.root>{children}</Styled.root>
+    </>
   </ThemeProvider>
 );
