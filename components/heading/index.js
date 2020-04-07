5104955680bd4ee0de86df7abdd35baa8ab7a1a1
Jacob Ruiz <jacobruizdesign@gmail.com>
2020-04-07 17:47:15 +1000

    add heading snippets

--- components/heading/index.js
+++ components/heading/index.js
@@ -1,9 +1,24 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
 import { Heading as Element } from 'theme-ui';
+import PropTypes from 'prop-types';
 
 const Heading = (props) => {
   return <Element {...props} />;
+};
+
+Heading.propTypes = {
+  variant: PropTypes.oneOf([
+    'heading.1',
+    'heading.2',
+    'heading.3',
+    'heading.4',
+    'heading.5',
+  ]),
+};
+
+Heading.defaultProps = {
+  variant: 'heading.1',
 };
 
 export default Heading;
--- playroom/snippets.js
+++ playroom/snippets.js
@@ -41,4 +41,40 @@ export default [
       <Button variant="destructive.secondary">Button</Button>
     `,
   },
+  // Headings
+  {
+    group: 'Heading',
+    name: 'Heading 1',
+    code: `
+      <Heading variant="heading.1">Heading 1</Heading>
+    `,
+  },
+  {
+    group: 'Heading',
+    name: 'Heading 2',
+    code: `
+      <Heading variant="heading.2">Heading 2</Heading>
+    `,
+  },
+  {
+    group: 'Heading',
+    name: 'Heading 3',
+    code: `
+      <Heading variant="heading.3">Heading 3</Heading>
+    `,
+  },
+  {
+    group: 'Heading',
+    name: 'Heading 4',
+    code: `
+      <Heading variant="heading.4">Heading 4</Heading>
+    `,
+  },
+  {
+    group: 'Heading',
+    name: 'Heading 5',
+    code: `
+      <Heading variant="heading.5">Heading 5</Heading>
+    `,
+  },
 ];
